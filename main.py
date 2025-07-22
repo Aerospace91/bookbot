0a0b2557b105dd6c5123305be1a65e627ab19268
Aerospaced91 <aerospaceosrs@gmail.com>
2025-07-22 09:02:06 -0700

    Pretty Book Report in Main

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-from stats import get_num_words, get_num_character
+from stats import get_num_words, get_num_character, sort_dict
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_char = get_num_character(text)
-    print(f"{num_words} words found in the document")
-    print(num_char)
+    sorted_char = sort_dict(num_char)
+    
+    print("============ BOOKBOT ============\n")
+    print(f"Analyzing book found at {book_path}\n")
+    print("----------- Word Count ----------\n")
+    print(f"Found {num_words} total words\n")
+    print("--------- Character Count -------\n")
+    for char in sorted_char:
+        if char["char"].isalpha():
+            print(f"{char['char']}: {char['num']}\n")
+    print("============= END ===============")    
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+    
     
 main()
--- stats.py
+++ stats.py
@@ -7,3 +7,14 @@ def get_num_words(book_string):
 def get_num_character(book_string):
     book_string = book_string.lower()
     return dict(Counter(book_string))
+
+def sort_dict(char_dict):
+    char_lst = []
+    def sort_on(items):
+        return items["num"]
+    
+    for char in char_dict:
+        char_lst.append({"char": char, "num": char_dict[char]})
+    
+    char_lst.sort(reverse=True, key=sort_on)
+    return char_lst
