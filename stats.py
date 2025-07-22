38b3b2f2ac08f73abcc477b2c3cbe71c2a1ed2c2
Aerospaced91 <aerospaceosrs@gmail.com>
2025-07-22 08:45:53 -0700

    Refactor Character Count function

--- stats.py
+++ stats.py
@@ -1,13 +1,9 @@
+from collections import Counter
+
 def get_num_words(book_string):
     word_lst = book_string.split()
     return len(word_lst)
 
 def get_num_character(book_string):
     book_string = book_string.lower()
-    character_dict = {}
-    for char in book_string:
-        if char in character_dict:
-            character_dict[char] += 1
-        else:
-            character_dict[char] = 1
-    return character_dict
+    return dict(Counter(book_string))
