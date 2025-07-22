158c6c155cc51a82250b1531a3a31db69637da4e
Aerospaced91 <aerospaceosrs@gmail.com>
2025-07-22 09:10:03 -0700

    Finalizing bookbot

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+import sys
 from stats import get_num_words, get_num_character, sort_dict
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_char = get_num_character(text)
     sorted_char = sort_dict(num_char)
     
-    print("============ BOOKBOT ============\n")
+    print("\n============ BOOKBOT ============\n")
     print(f"Analyzing book found at {book_path}\n")
     print("----------- Word Count ----------\n")
     print(f"Found {num_words} total words\n")
